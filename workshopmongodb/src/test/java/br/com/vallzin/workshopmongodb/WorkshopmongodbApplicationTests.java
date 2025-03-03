2647a76bd76304cd2ea76ba86fda6ea1bc3b3c4a
vallzin <valmilson@gmail.com>
2025-03-03 17:50:24 -0300

    primeiro commit - projeto criado

---
+++ workshopmongodb/src/test/java/br/com/vallzin/workshopmongodb/WorkshopmongodbApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.vallzin.workshopmongodb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WorkshopmongodbApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
