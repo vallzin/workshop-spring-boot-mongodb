ef13e448f7f83cc3c5516b0821402f10c0b6ec27
vallzin <valmilson@gmail.com>
2025-03-04 17:26:06 -0300

    Obtendo um usuário por id

--- workshopmongodb/src/main/java/br/com/vallzin/workshopmongodb/services/UserService.java
+++ workshopmongodb/src/main/java/br/com/vallzin/workshopmongodb/services/UserService.java
@@ -1,12 +1,14 @@
 package br.com.vallzin.workshopmongodb.services;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import br.com.vallzin.workshopmongodb.domain.User;
 import br.com.vallzin.workshopmongodb.repository.UserRepository;
+import br.com.vallzin.workshopmongodb.services.exception.ObjectNotFoundException;
 
 @Service
 public class UserService {
@@ -14,9 +16,14 @@ public class UserService {
 	@Autowired
 	private UserRepository repo;
 	
+	
 	public List<User> findAll(){
 		return repo.findAll();
 	}
 	
+	public User findById(String id) {
+        Optional<User> user = repo.findById(id);
+        return user.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado: ID = " + id));
+    }
 
 }
