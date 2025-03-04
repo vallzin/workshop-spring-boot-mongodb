941b80149d4b17f7feec28f3f460366d8f71f239
vallzin <valmilson@gmail.com>
2025-03-04 18:04:22 -0300

    inserção de usuário com POST

--- workshopmongodb/src/main/java/br/com/vallzin/workshopmongodb/services/UserService.java
+++ workshopmongodb/src/main/java/br/com/vallzin/workshopmongodb/services/UserService.java
@@ -7,6 +7,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import br.com.vallzin.workshopmongodb.domain.User;
+import br.com.vallzin.workshopmongodb.dto.UserDTO;
 import br.com.vallzin.workshopmongodb.repository.UserRepository;
 import br.com.vallzin.workshopmongodb.services.exception.ObjectNotFoundException;
 
@@ -25,5 +26,13 @@ public class UserService {
         Optional<User> user = repo.findById(id);
         return user.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado: ID = " + id));
     }
+	
+	public User insert(User obj) {
+		return repo.insert(obj);
+	}
+	
+	public User fromDTO(UserDTO objDto) {
+		return new User(objDto.getId(), objDto.getName(), objDto.getEmail());
+	}
 
 }
